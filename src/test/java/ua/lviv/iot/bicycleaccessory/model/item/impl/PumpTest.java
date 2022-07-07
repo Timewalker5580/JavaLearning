a9dc95be64053d285411700e2202ad0667c3deca
Timewalker5580 <shaposhnikov5580@gmail.com>
2022-07-07 20:10:47 +0300

    minor refactoring

--- src/test/java/ua/lviv/iot/bicycleaccessory/model/item/impl/PumpTest.java
+++ src/test/java/ua/lviv/iot/bicycleaccessory/model/item/impl/PumpTest.java
@@ -1,4 +1,30 @@
 package ua.lviv.iot.bicycleaccessory.model.item.impl;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PumpTest {
+    private static final String NAME = "Test";
+    private static final String MANUFACTURER = "TestManufacturer";
+    private static final double PRICE = 234;
+    private static final double WEIGHT = 2.44;
+    private static final double PRESSURE = 233.1;
+
+    private Pump pump;
+
+    @BeforeEach
+    void setup() {
+        pump = new Pump(NAME, MANUFACTURER, PRICE, WEIGHT, PRESSURE);
+    }
+
+    @Test
+    void basicConstructorWithGetters() {
+        assertEquals(pump.getName(), NAME);
+        assertEquals(pump.getManufacturer(), MANUFACTURER);
+        assertEquals(pump.getPrice(), PRICE);
+        assertEquals(pump.getWeight(), WEIGHT);
+        assertEquals(pump.getPressure(), PRESSURE);
+    }
 }
